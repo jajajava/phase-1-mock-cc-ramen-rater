938e2211cfed647af51dff4ae267402be239f55e
David <davidstepanov@yahoo.com>
2022-09-07 11:28:59 -0400

    mock cc submission

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-// write your code here
+window.addEventListener('DOMContentLoaded', fetches())
+
+//submit button & event listener
+let submit = document.getElementById("sub")
+submit.addEventListener('onsubmit', console.log('submitted'))
+
+// let card= {
+//     Name: e.target.new-name.value,
+//     Restaurant: e.target.new-restaurant.value,
+//     Image: e.target.new-image.value,
+//     Rating: e.target.new-rating.value,
+//     Comment: e.target.new-comment.value
+// }   
+
+//create function 
+//NEEDS WORK!!               //diff class for CSS?
+function create(e){
+    e.preventDefault
+    
+}
+
+function fetches(){
+    fetch('http://localhost:3000/ramens')
+    .then((res)=>(res.json()))
+    .then((res)=>{res.forEach((i)=>(renderCard(i)))}
+)}
+//document.createElement('div').innerHTML(i)
+
+function renderCard(i){
+   let div = document.createElement('div')
+    div.createTextNode = i
+}
+console.log(renderCard())
+//I understand what I need to do theoretically, but I don't
+//get why I can't create an element in HTML. I think I'm
+//using the right commands. 
+
